9a8b767b7a7019dbe4c7228c968f26c02f9c2797
leo60228 <leo@60228.dev>
2021-01-26 14:14:53 -0500

    add Rust to flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,26 @@
 {
-  inputs.pebble.url = "github:Sorixelle/pebble.nix";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pebble = {
+    url = "github:Sorixelle/pebble.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, pebble, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell = pebble.pebbleEnv.${system} {};
-    });
+  outputs = { self, pebble, flake-utils, rust-overlay, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+      in {
+        devShell = pebble.pebbleEnv.${system} {
+          nativeBuildInputs = [ rust ];
+        };
+      });
 }
